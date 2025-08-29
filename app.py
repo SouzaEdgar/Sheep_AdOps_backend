86b87df38ed86545764ef80ed07365ad4eaa2893
SouzaEdgar <edgarss2013@gmail.com>
2025-08-29 19:10:33 -0300

    fix: importação Request de fastapi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from routes import main_routes
 from fastapi.responses import JSONResponse
